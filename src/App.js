4760877f8b06962ae34b6e5c0e2d19a9047e256d
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-02 16:02:41 +0200

    done with static setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 function App() {
   return (
     <Router>
-      
-      <div className='flex flex-col justify-between h-screen'>
-      <Navbar></Navbar>
-      <main className='container mx-auto px-3 pb-12'> Content</main>
-      <Footer></Footer>
-      </div>
 
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+
+            <Route path="/notfound" element={<NotFound></NotFound>}></Route>
+            <Route path="/*" element={<NotFound></NotFound>}></Route>
+
+            <Route path="/about" element={<About></About>}></Route>
+
+
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
     </Router>
-    
+
   );
 }
 
