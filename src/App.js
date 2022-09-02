f22dd46dc59ffedc0c625a6cf28eba6625c33451
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-02 15:17:53 +0200

    initialize Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <Router>
       
-    
-    </div>
+      <div className='flex flex-col justify-between h-screen'>
+      <Navbar></Navbar>
+      <main> Content</main>
+
+      </div>
+
+    </Router>
     
   );
 }
