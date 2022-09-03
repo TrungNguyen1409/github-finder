5eaee73e3ea27ad721039b1ea725c529c78b9076
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-03 17:09:40 +0200

    take care of the loading state before implementing search component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { useContext } from 'react'
-import { useEffect } from 'react'
 import Spinner from '../layout/assets/Spinner'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
     
-    const {users, loading, fetchUsers} = useContext(GithubContext)
-
-    useEffect(() => {
-        fetchUsers()
-    },[])
+    const {users, loading} = useContext(GithubContext)
 
     
 
-    if (!loading) {
+    
+
+    if (loading === false) {
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md: grid-cols-2'>
                 {users.map((user) => (
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,4 @@
-import { createContext , useReducer } from "react"
+import { createContext, useReducer } from "react"
 import githubReducer from "./GithubReducer"
 
 
@@ -7,15 +7,19 @@ const GithubContext = createContext()
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-export const GithubProvider = ({children}) => {
-    const initialState={
-        users:[],
-        loading: true
+export const GithubProvider = ({ children }) => {
+    const initialState = {
+        users: [],
+        loading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
+
+    // Get inital users (testing purposes)
     const fetchUsers = async () => {
+        setLoading()
+
         const response = await fetch(`${GITHUB_URL}/users`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
@@ -28,6 +32,11 @@ export const GithubProvider = ({children}) => {
             payload: data
         })
     }
+
+    // Set Loading
+    const setLoading = () => dispatch({type:
+        'SET_LOADING'
+    })
 
     return <GithubContext.Provider value={{
         users: state.users,
