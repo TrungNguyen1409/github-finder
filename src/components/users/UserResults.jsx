68e5f64d3e52f492db777dbc1cecbbd50c7b29a6
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-03 13:02:26 +0200

    git ignore

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import Spinner from '../layout/assets/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
     const [users, setUsers] = useState([])
@@ -17,32 +18,23 @@ function UserResults() {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
         })
-
         const data = await response.json()
-
         setUsers(data)
         setLoading(false)
-        console.log(data)
-
     }
 
 
     if (!loading) {
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md: grid-cols-2'>
-
                 {users.map((user) => (
-                    <h3>{user.login}</h3>
-
+                    <UserItem key={user.id} user={user}></UserItem>
                 ))}
-
             </div>
         )
     } else {
         return <Spinner></Spinner>
     }
-
-
 }
 
 export default UserResults
