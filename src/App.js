51eb3997971867439d17c6b46176b43fa02304fc
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-05 13:16:59 +0200

    implement error message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
+
 import Navbar from './components/layout/Navbar';
 import { GithubProvider } from './context/github/GithubContext'
 import {AlertProvider} from './context/alert/AlertContext'
@@ -16,7 +18,7 @@ function App() {
         <div className='flex flex-col justify-between h-screen'>
           <Navbar />
           <main className='container mx-auto px-3 pb-12'>
-
+            <Alert/>
             <Routes>
               <Route path="/" element={<Home></Home>}></Route>
               <Route path="/notfound" element={<NotFound></NotFound>}></Route>
--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -1,25 +1,30 @@
 import React from 'react'
-import {createContext, useReducer} from 'react'
+import { createContext, useReducer } from 'react'
 import alertReducer from './AlertReducer'
 
 const AlertContext = createContext()
 
-export const AlertProvider = ({children}) => {
+export const AlertProvider = ({ children }) => {
     const initialState = null
 
-    const [state,dispatch] = useReducer(alertReducer, initialState)
+    const [state, dispatch] = useReducer(alertReducer, initialState)
 
     // Set an alert
-    const setAlert = (msg,type)=>{
+    const setAlert = (msg, type) => {
         dispatch({
             type: 'SET_ALERT',
-            payload: {msg, type}
+            payload: { msg, type }
         })
 
-        setTimeout(() => dispatch({type: 'REMOVE_ALERT'}),3000)
+        setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000)
     }
 
-    return <AlertContext.Provider value={{alert: state,setAlert}}>
+    return <AlertContext.Provider value={
+        {
+            alert: state,
+            setAlert
+        }
+    }>
         {children}
     </AlertContext.Provider>
 }
