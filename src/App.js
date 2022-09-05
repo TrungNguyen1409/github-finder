22b48342120a5bf1ea89b46fa3026e11c00f86ca
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-05 13:28:03 +0200

    start implementing getSingleUser function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
-import Alert from './components/layout/Alert';
+import User from './pages/User';
 
+import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import { GithubProvider } from './context/github/GithubContext'
 import {AlertProvider} from './context/alert/AlertContext'
@@ -20,10 +21,11 @@ function App() {
           <main className='container mx-auto px-3 pb-12'>
             <Alert/>
             <Routes>
-              <Route path="/" element={<Home></Home>}></Route>
+              <Route path="/" element={<Home/>}></Route>
               <Route path="/notfound" element={<NotFound></NotFound>}></Route>
               <Route path="/*" element={<NotFound></NotFound>}></Route>
               <Route path="/about" element={<About></About>}></Route>
+              <Route path="/user/:login" element={<User/>}></Route>
             </Routes>
           </main>
           <Footer></Footer>
