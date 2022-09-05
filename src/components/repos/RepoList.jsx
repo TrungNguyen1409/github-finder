5c99f562dc96590c65e5fe02ce757d9792147a92
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-09-05 19:03:46 +0200

    get lates repos

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -6,7 +6,7 @@ function RepoList({repos}) {
     <div className='rounded-lg shadow-lg card bg-base-100'>
         <div className="card-bod">
             <h2 className="text-3xl my-4 font-bold card-title">
-                Top Repositories
+                Latest Repositories
             </h2>
             {repos.map((repo)=>(
                 <h3>{repo.name}</h3>
